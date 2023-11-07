554069aed7a55a022fdbdd58a43bc89a30b25a5d
Yang <junsuk530@gmail.com>
2023-11-07 10:34:45 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20200694/poker/MainActivity.kt
@@ -0,0 +1,15 @@
+package kr.ac.kumoh.ce.s20200694.poker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.ce.s20200694.poker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+        main.card1.setImageResource(R.drawable.c_2_of_hearts)
+    }
+}
