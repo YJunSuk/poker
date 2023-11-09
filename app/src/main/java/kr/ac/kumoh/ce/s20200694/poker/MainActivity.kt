cd6e8eed2155a9eda7aba56c4c7f2cbd9b70639b
Yang <junsuk530@gmail.com>
2023-11-09 20:38:20 +0900

    test3

--- app/src/main/java/kr/ac/kumoh/ce/s20200694/poker/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20200694/poker/MainActivity.kt
@@ -2,14 +2,64 @@ package kr.ac.kumoh.ce.s20200694.poker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20200694.poker.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var main: ActivityMainBinding;
+    private lateinit var main: ActivityMainBinding
+    private lateinit var model: PokerViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
-        main.card1.setImageResource(R.drawable.c_2_of_hearts)
+
+        model = ViewModelProvider(this)[PokerViewModel::class.java]
+        model.cards.observe(this, Observer{
+            val res = IntArray(5)
+            for (i in it.indices) {
+                res[i] = resources.getIdentifier(
+                    getCardName(it[i]),
+                    "drawable",
+                    packageName
+                )
+                Log.i("Test", "$i : ${getCardName(it[i])}")
+            }
+            main.card1.setImageResource(res[0])
+            main.card2.setImageResource(res[1])
+            main.card3.setImageResource(res[2])
+            main.card4.setImageResource(res[3])
+            main.card5.setImageResource(res[4])
+        })
+        main.button.setOnClickListener {
+            model.shuffle()
+        }
+
+    }
+    private fun getCardName(c: Int) : String{
+        var shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        if (c % 13 in 10..12)
+            return "c_${number}_of_${shape}2"
+        else
+          return "c_${number}_of_${shape}"
     }
 }
+
+
--- app/src/main/java/kr/ac/kumoh/ce/s20200694/poker/PokerViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20200694/poker/PokerViewModel.kt
@@ -1,10 +1,79 @@
 package kr.ac.kumoh.ce.s20200694.poker
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
 class PokerViewModel : ViewModel() {
+    private var _cards = MutableLiveData<IntArray>(IntArray(5) { -1 })
+    val cards: LiveData<IntArray>
+        get() = _cards
+    fun shuffle() {
+        var num = 0
+        val newCards = IntArray(5) { -1 }
+
+        for (i in newCards.indices) {
+            var isDuplicate: Boolean
+            do {
+                num = Random.nextInt(52)
+                isDuplicate = false
+                for (j in 0 until i) {
+                    if (newCards[j] == num) {
+                        isDuplicate = true
+                        break
+                    }
+                }
+            } while (isDuplicate)
+            newCards[i] = num
+        }
+
+        // 정렬
+        newCards.sort()
+
+        _cards.value = newCards
+    }
+    fun HandRankings(): String{
+        val cards = _cards.value ?: return "카드가 없음"
+
+        if (cards.size != 5){
+            return "카드가 5장이 아님"
+        }
+
+        cards.sort() //카드 정렬
+
+        //포커족보 평가 로직
+        if(isRoyalFlush(cards)) {
+            return "로열 플러시"
+        }else if(isBackStraightFlush(cards)) {
+            return "백 스트레이트 플러시"
+        }
+        else if (isStraightFlush(cards)) {
+            return "스트레이트 플러시"
+        } else if (isFourOfAKind(cards)) {
+            return "포카드"
+        } else if (isFullHouse(cards)) {
+            return "풀하우스"
+        } else if (isFlush(cards)) {
+            return "플러시"
+        } else if (isMountain(cards)) {
+            return "마운틴"
+        }else if (isStraight(cards)) {
+            return "스트레이트"
+        }else if (isBackStraight(cards)) {
+            return "백 스트레이트"
+        } else if (isThreeOfAKind(cards)) {
+            return "트리플"
+        } else if (isTwoPair(cards)) {
+            return "투페어"
+        } else if (isOnePair(cards)) {
+            return "원페어"
+        } else {
+            return "탑"
+        }
+    }
 
 
+    }
 }
